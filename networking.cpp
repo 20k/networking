5bcd6f0c0d41f2ebcce16b3f6927110b1d8e65d1
James <Icedshot@gmail.com>
2019-12-20 13:19:48 +0000

    increase sleep to lower cpu usage

--- networking.cpp
+++ networking.cpp
@@ -727,7 +727,7 @@ void client_thread_tcp(connection& conn, std::string address, uint16_t port)
                 }
             }
 
-            std::this_thread::sleep_for(std::chrono::milliseconds(1));
+            std::this_thread::sleep_for(std::chrono::milliseconds(8));
         }
 
     }
