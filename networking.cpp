2e20c417bc8a41445567be0510bd1bf67eaced2b
James <Icedshot@gmail.com>
2020-10-17 19:42:26 +0100

    only wake if something actually happens

--- networking.cpp
+++ networking.cpp
@@ -775,9 +775,8 @@ void server_thread(connection& conn, std::string saddress, uint16_t port, connec
                     if(it->second.current_state != session_data<T>::err && it->second.current_state != session_data<T>::terminated)
                     {
                         it->second.current_state = session_data<T>::err;
+                        wake_queue.push_back(it->first);
                     }
-
-                    wake_queue.push_back(it->first);
                 }
             }
 
